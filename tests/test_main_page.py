2d1953bee08e767bd8b28471bb7cb6e819122f1d
HEHABUSTb <msanvold@gmail.com>
2021-11-07 18:40:42 +0300

    add allure support in tests add allure screenshot in base_page should_be_login_link test

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,15 +1,28 @@
+import allure
+
 from pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 import pytest
-import allure_pytest
-import allure
+
 
 link = 'http://selenium1py.pythonanywhere.com/' #main_page link
 
 @pytest.mark.smoke
-def test_guest_can_add_item_in_basket(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.add_book_in_basket()
-    page.basket_is_not_empty()
+@allure.severity(allure.severity_level.NORMAL)
+class TestSmokeMainPage():
+    def test_guest_can_add_item_in_basket(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.add_book_in_basket()
+        page.basket_is_not_empty()
+
+    def test_guest_can_passed_registration(self, browser):
+        pytest.skip('Just example skip test for allure')
+
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_guest_should_see_login_link(self, browser): #example failed test login link invalid
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
