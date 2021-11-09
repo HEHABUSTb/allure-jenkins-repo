9d02d2d6f19b690eb3bc0d9fad1d232b8661ab0c
HEHABUSTb <msanvold@gmail.com>
2021-11-09 13:59:00 +0300

    add allure description

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -10,16 +10,19 @@ link = 'http://selenium1py.pythonanywhere.com/' #main_page link
 @pytest.mark.smoke
 @allure.severity(allure.severity_level.NORMAL)
 class TestSmokeMainPage():
+    @allure.description('Guest can add book in basket')
     def test_guest_can_add_item_in_basket(self, browser):
         page = MainPage(browser, link)
         page.open()
         page.add_book_in_basket()
         page.basket_is_not_empty()
 
+    @allure.description('TEst')
     def test_guest_can_passed_registration(self, browser):
         pytest.skip('Just example skip test for allure')
 
     @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description('Guest should see login link')
     def test_guest_should_see_login_link(self, browser): #example failed test login link invalid
         page = MainPage(browser, link)
         page.open()
